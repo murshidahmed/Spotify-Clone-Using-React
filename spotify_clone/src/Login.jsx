7bf9d905c8efedd282488ff1297fc7629702b492
murshidahmed <murshidahmedneon3@gmail.com>
2025-07-01 07:46:58 +0530

    first commit

---
+++ spotify_clone/src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./login.css"
+
+function Login() {
+  return (
+    <div className=' flex justify-center'>
+        <div className='w-24'> 
+            <img src="/my mj.jpg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Login
